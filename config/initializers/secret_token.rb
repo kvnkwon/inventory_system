b28df75f86d4bb57696e3c31ae73521b0ca8a415
Kevin <kvnkwon@gmail.com>
2014-01-05 17:48:19 -0500

    started feature record inventory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InventorySystem::Application.config.secret_key_base = '4accd11e61eab1704b2b52457e3522fff06c92a2ff96e5674c500784948233b364a2c7048d627aac5e13c4375f37c5c112573a7e01337008b8865acf98587d84'
