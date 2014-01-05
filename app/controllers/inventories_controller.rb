3c7ea8b0a3d8ef0e656d6b87b302eea7ac900d9c
Kevin <kvnkwon@gmail.com>
2014-01-05 18:17:59 -0500

    got passing tests for creating a new inventory with valid attributes

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -5,7 +5,19 @@ class InventoriesController < ApplicationController
   end
 
   def create
+    @inventory = Inventory.new(inventory_params)
+    if @inventory.save
+      redirect_to new_inventory_path
+    else
+      flash.now[:error] = "Cannot create inventory"
+      render :new
+    end
   end
 
+  private
+
+    def inventory_params
+      params.require(:inventory).permit(:title, :description, :quantity)
+    end
 
 end
--- spec/features/user_creates_an_item_spec.rb
+++ spec/features/user_creates_an_item_spec.rb
@@ -4,10 +4,18 @@ feature 'User creates an item' do
 
   it "with valid attributes" do
     visit new_inventory_path
+    fill_in "Title", with: "Steak"
+    fill_in "Description", with: "Big and juicy"
+    fill_in "Quantity", with: "2"
+    click_on "Create Inventory"
+
+    expect(page).to have_content "Title"
   end
 
   it "with invalid attributes and sees errors" do
     visit new_inventory_path
+    click_on "Create Inventory"
+    expect(page).to have_content "Cannot create inventory"
   end
 
 
